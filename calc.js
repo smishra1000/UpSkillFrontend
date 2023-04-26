810b799055a3773af3a5528f925021b0e5ef0dc3
Sameer Mishra <sameer_mishra@epam.com>
2023-04-26 18:50:04 +0530

    upskill training

---
+++ calc.js
@@ -0,0 +1,16 @@
+export function sum(a,b){
+return a+b
+}
+
+
+export function subtract(a,b){
+    return a-b;
+}
+
+export default function multiply(a,b){
+    return a*b
+}
+
+export function getName(user){
+    return user.name
+}
---
+++ mainentry.js
@@ -0,0 +1,16 @@
+// import {sum as add} from "./calc.js"
+
+// import { subtract as sub} from "./calc.js"
+
+// import multi from "./calc.js"
+
+// import {getName} from "./calc.js"
+
+import multi, {sum,subtract,getName} from "./calc.js"
+
+console.log(sum(10,20))
+console.log(subtract(30,50))
+
+console.log(multi(30,40))
+
+console.log(getName({name:"sameer"}))
